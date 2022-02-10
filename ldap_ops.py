2877e6487e59049a2bd75be18aaa7c1789223d3f
Michael Delgado <devmikedel@gmail.com>
2022-02-09 22:11:54 -0600

    Update ldap_ops.py with get_all_emails method

--- ldap_ops.py
+++ ldap_ops.py
@@ -1,6 +1,5 @@
-import ldap, os, smbpasswd, base64, hashlib
-import ldap.modlist as modlist
-
+from collections import OrderedDict
+import ldap
 
 LOGIN_DN = 'cn=read-only-admin,dc=example,dc=com'
 LOGIN_PASSWORD = 'password'
@@ -13,7 +12,7 @@ class LDAPOps(object):
         self.BIND_DN = LOGIN_DN
         self.BIND_PASSWORD = LOGIN_PASSWORD
         self.base_dn = 'dc=example, dc=com'
-        self.ldap_connection = ldap.initialize(server_address)
+        self.ldap_connection = ldap.initialize(SERVER_ADDRESS)
         try:
             self.ldap_connection.simple_bind_s(self.BIND_DN, self.BIND_PASSWORD)
         except Exception as e:
@@ -55,3 +54,34 @@ class LDAPOps(object):
                 ldap.SCOPE_SUBTREE, 
                 )
         return [x for x in results]
+
+    def get_all_persons(self):
+        try:
+            all_persons = self.ldap_connection.search_s(
+                    self.base_dn,
+                    ldap.SCOPE_SUBTREE,
+                    ('objectClass=person'),
+                    )
+        except KeyError:
+            pass
+        return all_persons
+
+    def get_all_emails(self):
+        try:
+            all_emails = self.ldap_connection.search_s(
+                    self.base_dn,
+                    ldap.SCOPE_SUBTREE,
+                    ('objectClass=person'),
+                    ['mail'],
+                    )
+        except Exception:
+            print(Exception)
+        all_emails_clean = []
+        errors = []
+        for email in all_emails:
+            try:
+                all_emails_clean.append(email[1]['mail'][0].decode())
+            except Exception as e:
+                errors.append(email[0])
+        return {'emails':all_emails_clean,'errors':errors}
+
